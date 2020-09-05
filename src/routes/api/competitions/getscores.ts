eb468fca877758fc2eb290202cb90239c66c4c5d
Jimmy Li <jimmyli2002@gmail.com>
2020-09-05 20:17:14 +0000

    Added stronger typing to getscores route

--- src/routes/api/competitions/getscores.ts
+++ src/routes/api/competitions/getscores.ts
@@ -3,8 +3,8 @@ import Ajv from 'ajv';
 
 import { JSONResponse, ResponseStatus } from '../../../ts/types';
 
-import competition from '../../../database/models/competition';
-import score from '../../../database/models/score';
+import competition, { ICompetition } from '../../../database/models/competition';
+import score, { IScore } from '../../../database/models/score';
 
 const ajv = new Ajv({ allErrors: true });
 
@@ -18,6 +18,14 @@ const ajv = new Ajv({ allErrors: true });
     status: boolean;
     checksUp: number;
     totalChecks: number;
+ }
+
+ interface TeamScores {
+     [key: string]: DetailedServiceScore;
+ }
+
+ interface Scores {
+     [key: string]: TeamScores;
  }
 
 const schema = {
@@ -36,14 +44,14 @@ const handler = async (req: Request, res: Response): Promise<any> => {
     const body = req.body;
     if(validate(body)){
         // Validate that the competition exists before getting information
-        const competitionObject = await competition.findOne({ comp_id: body.compId });
+        const competitionObject: ICompetition = await competition.findOne({ comp_id: body.compId });
         if(!competitionObject) return res.json({ status: ResponseStatus.error, data: 'the competition id could not be found' } as JSONResponse);
         // Validate that the requester is a member of the competition; Owner is added to users so this will work for all
         if(competitionObject.users.includes(req.auth.username)){
-            const scoreRecords = await score.find({ comp_id: body.compId }, null, { sort: { createdAt: 'desc' } });
+            const scoreRecords: IScore[] = await score.find({ comp_id: body.compId }, null, { sort: { createdAt: 'desc' } });
 
             // Process the records to have useful data; TODO: Offload this to an information calc worker and implement a server-side cache
-            const scores: any = {}; // TODO: Type this stronger because it is like a map
+            const scores: Scores = {};
 
             for(const scoreRecord of scoreRecords){
                 if(scores[scoreRecord.teamname] === undefined){
